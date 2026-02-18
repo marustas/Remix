b3437bdf4cf5a87ef2982fbf1ebeb64a4c26e3de
marustas <marudov.stas@gmail.com>
2026-02-18 12:13:35 +0200

    feat(contacts): route for contacts

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,11 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/hello", "routes/hello.tsx"),
+  route("contacts/:contactId", "routes/contacts.tsx"),
 ] satisfies RouteConfig;
