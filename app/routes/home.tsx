3f873a41950d18f9cc3a07c084fc7c4e93e5f976
stastone <StanislauMarudau@coherentsolutions.com>
2026-02-18 11:49:45 +0200

    feat(home): use meta function

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,13 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { MetaFunction } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
+export const meta: MetaFunction = () => {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
+    {},
   ];
-}
+};
 
 export default function Home() {
   return <Welcome />;
